cfcc2b1604468254a30f19caef974cc35633405f
marcos-burghausen <marcosrb21@gmail.com>
2025-01-23 21:19:07 -0300

    Ajustes na view carteiras

--- frontend/src/views/contas/ContasView.vue
+++ frontend/src/views/contas/ContasView.vue
@@ -1,29 +1,16 @@
 <template>
   <div class="content-wrapper">
     <!-- <div class="pagetitle"> -->
-    <nav class="d-flex ms-3 mb-3">
-      <span class=" me-3 text-white">
-        <router-link
-          class="link"
-          :to="{ name: 'dashboard' }"
-        >
-          <mdicon
-            name="arrow-left"
-            size="20"
-          />
+    <nav class="d-flex ms-3 mb-3 mt-3">
+      <span class="me-3 text-white">
+        <router-link class="link" :to="{ name: 'dashboard' }">
+          <mdicon name="arrow-left" size="20" />
         </router-link>
       </span>
-      <span
-        class="text-white"
-        style="font-size: 20px;"
-      >
-        Contas
-      </span>
+      <span class="text-white" style="font-size: 20px"> Contas </span>
     </nav>
     <!-- </div> -->
-    <div
-      class="mobile"
-    >
+    <div class="mobile">
       <div class="body__carteira">
         <div class="saldo">
           <span class="saldo__atual">saldo atual</span>
@@ -34,41 +21,23 @@
           <span class="valor">R$ 130,00</span>
         </div>
       </div>
-      <div
-        v-for="(wallet, index) in wallets"
-        :key="index"
-        class="carteira"
-      >
+      <div v-for="(wallet, index) in wallets" :key="index" class="carteira">
         <div class="header__carteira">
           <div class="container__detalhes">
             <span class="icon">
-              <mdicon
-                :name="wallet.icon"
-                size="50"
-              />
+              <mdicon :name="wallet.icon" size="50" />
             </span>
             {{ wallet.name }}
           </div>
           <button class="btn__opcoes">
-            <mdicon
-              name="dots-vertical"
-              size="25"
-            />
+            <mdicon name="dots-vertical" size="25" />
           </button>
         </div>
         <div class="body__carteira">
           <div class="saldo">
             <span class="saldo__atual">saldo atual</span>
-            <span
-              v-if="wallet.saldo == null"
-              class="valor"
-            >
-              R$ 0,00
-            </span>
-            <span
-              v-else
-              class="valor"
-            >
+            <span v-if="wallet.saldo == null" class="valor"> R$ 0,00 </span>
+            <span v-else class="valor">
               R$ {{ formatValue(wallet.saldo) }}
             </span>
           </div>
@@ -90,9 +59,7 @@
     </div>
 
     <div class="pc">
-      <div
-        class="container__cards"
-      >
+      <div class="container__cards">
         <div class="card__new__conta">
           <div class="btn__new__conta">
             <!-- <div class="plus"> -->
@@ -103,24 +70,18 @@
             <ModalNovaConta />
             <!-- </div> -->
             <span class="add__conta">Criar conta</span>
-          </div> 
+          </div>
         </div>
         <div class="carteira">
           <div class="header__carteira">
             <div class="container__detalhes">
               <span class="icon">
-                <mdicon
-                  name="cash"
-                  size="35"
-                />
+                <mdicon name="cash" size="35" />
               </span>
               carteira
             </div>
             <button class="btn__opcoes">
-              <mdicon
-                name="dots-vertical"
-                size="35"
-              />
+              <mdicon name="dots-vertical" size="35" />
             </button>
           </div>
           <div class="body__carteira">
@@ -134,9 +95,7 @@
             </div>
           </div>
           <div class="footer__carteira">
-            <button class="btn__add__despesa">
-              ADICIONAR DESPESA
-            </button>
+            <button class="btn__add__despesa">ADICIONAR DESPESA</button>
           </div>
         </div>
       </div>
@@ -164,165 +123,163 @@ import { ref } from "vue";
 const useWallets = useWalletsStore();
 let wallets = ref(useWallets.walletsData.wallets.wallets);
 const updateContas = (novoValor) => {
-    wallets.value = novoValor;
+  wallets.value = novoValor;
 };
-
 </script>
 
 <style scoped>
- .link {
-      text-decoration: none;
-      color: #fefefe;
-  }
-  .opaco {
-      color: #6c757d !important;
-  }
-  .container__cards {
-    width: 66%;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    padding-inline: 15px;
-  }
-  .card__new__conta {
-    height: 248px;
-    width: 49%;
-    margin-block: 10px;
-    border-radius: 15px;
-    box-shadow: -4px -4px 5px #3e4247, 7px 7px 7px #1d1f23;
-    display: flex;
-    justify-content: center;
-    align-items: center;      
-  }
-  .btn__new__conta {
-    background: transparent;
-    height: 50%;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    cursor: pointer;
-    color: #77d08e;
-    font-weight: 100;
-  }
-  .plus {
-    height: 68px;
-    width: 68px;
-    border: solid 1px #77d08e;
-    border-radius: 50px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  .add__conta {
-    font-size: 20px;
-    font-weight: 400;
-    color: #77d08e;
-  }
-  .carteira {
-    box-shadow: -4px -4px 5px #3e4247, 7px 7px 7px #1d1f23;
-    height: 248px;
-    width: 49%;
-    margin-block: 10px;
-    border-radius: 15px;
-    display: flex;
-    flex-direction: column;
-    padding-inline: 10px;
-  }
-  .header__carteira {
-    height: 25%;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
-  .container__detalhes {
-    width: 320px;
-    height: 52px;
-    background: transparent;
-    color: #fefefe;
-    font-size: 30px;
-    padding-left: 15px;
-    cursor: pointer;
-  }
-  .icon {
-    color: #77d08e;
-  }
-  .btn__opcoes {
-    height: 52px;
-    width: 52px;
-    border-radius: 30px;
-    color: white;
-  }
-  .btn__opcoes:hover {
-    background-color: rgba(254, 254, 254, 0.1);
-  }
-  .body__carteira {
-    height: 50%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    padding-inline: 15px;
-  }
-  .saldo {
-    display: flex;
-    justify-content: space-between;
+.link {
+  text-decoration: none;
+  color: #fefefe;
+}
+.opaco {
+  color: #6c757d !important;
+}
+.container__cards {
+  width: 66%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  padding-inline: 15px;
+}
+.card__new__conta {
+  height: 248px;
+  width: 49%;
+  margin-block: 10px;
+  border-radius: 15px;
+  box-shadow: -4px -4px 5px #3e4247, 7px 7px 7px #1d1f23;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.btn__new__conta {
+  background: transparent;
+  height: 50%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  color: #77d08e;
+  font-weight: 100;
+}
+.plus {
+  height: 68px;
+  width: 68px;
+  border: solid 1px #77d08e;
+  border-radius: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.add__conta {
+  font-size: 20px;
+  font-weight: 400;
+  color: #77d08e;
+}
+.carteira {
+  box-shadow: -4px -4px 5px #3e4247, 7px 7px 7px #1d1f23;
+  height: 248px;
+  width: 49%;
+  margin-block: 10px;
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  padding-inline: 10px;
+}
+.header__carteira {
+  height: 25%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.container__detalhes {
+  width: 320px;
+  height: 52px;
+  background: transparent;
+  color: #fefefe;
+  font-size: 30px;
+  padding-left: 15px;
+  cursor: pointer;
+}
+.icon {
+  color: #77d08e;
+}
+.btn__opcoes {
+  height: 52px;
+  width: 52px;
+  border-radius: 30px;
+  color: white;
+}
+.btn__opcoes:hover {
+  background-color: rgba(254, 254, 254, 0.1);
+}
+.body__carteira {
+  height: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  padding-inline: 15px;
+}
+.saldo {
+  display: flex;
+  justify-content: space-between;
+}
+.saldo__atual {
+  font-size: 20px;
+  color: #fefefe;
+}
+.valor {
+  font-size: 20px;
+  color: #06bb64;
+}
+.footer__carteira {
+  height: 25%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.btn__add__despesa {
+  color: #77d08e;
+  padding: 10px;
+  border-radius: 25px;
+}
+.btn__add__despesa:hover {
+  background-color: rgba(254, 254, 254, 0.1);
+}
+.container__card__atual_previsto {
+  width: 33%;
+  height: 248px;
+  margin-block: 10px;
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.card__valor {
+  box-shadow: -4px -4px 5px #3e4247, 7px 7px 7px #1d1f23;
+  height: 110px;
+  width: 100%;
+  border-radius: 15px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-inline: 15px;
+}
+.saldo__card {
+  color: #fefefe;
+  font-size: 25px;
+}
+.valor__card {
+  color: #06bb64;
+  font-size: 25px;
+}
+.pc {
+  display: flex;
+}
 
-  }
-  .saldo__atual {
-    font-size: 20px;
-    color: #fefefe;
-  }
-  .valor {
-    font-size: 20px;
-    color: #06bb64;
-  }
-  .footer__carteira {
-    height: 25%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  .btn__add__despesa {
-    color: #77d08e;
-    padding: 10px;
-    border-radius: 25px;
-  }
-  .btn__add__despesa:hover {
-    background-color: rgba(254, 254, 254, 0.1);
-  }
-  .container__card__atual_previsto {
-    width: 33%;
-    height: 248px;
-    margin-block: 10px;
-    border-radius: 15px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-  }
-  .card__valor {
-    box-shadow: -4px -4px 5px #3e4247, 7px 7px 7px #1d1f23;
-    height: 110px;
-    width: 100%;
-    border-radius: 15px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding-inline: 15px;
-  }
-  .saldo__card {
-    color: #fefefe;
-    font-size: 25px;
-  }
-  .valor__card {
-    color: #06bb64;
-    font-size: 25px;
-  }
-  .pc {
-    display: flex;
-  }
-
-  @media screen and (max-width: 600px) {
+@media screen and (max-width: 600px) {
   .pc {
     display: none;
   }
@@ -339,7 +296,7 @@ const updateContas = (novoValor) => {
     padding: 0 10px;
     display: flex;
     justify-content: center;
-    align-items: end;      
+    align-items: end;
   }
   .container__detalhes {
     font-size: 25px;
@@ -347,6 +304,5 @@ const updateContas = (novoValor) => {
   .body__carteira {
     margin: 20px 0 30px 0;
   }
-      
 }
-  </style>
+</style>
