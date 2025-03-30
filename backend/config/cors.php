24508f4c29756f50d5d79fdcc606b12ec8634270
marcos-burghausen <rafaelburghausen@gmail.com>
2025-03-30 01:49:20 -0300

    ajustando url do front no back

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:4081'],
+    'allowed_origins' => ['https://mrfinancas.burghausen.com.br'],
 
     'allowed_origins_patterns' => [],
 
