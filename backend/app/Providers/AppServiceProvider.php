065c9d096c80f63fecd9956f0bdf3539c82212ab
marcos-burghausen <marcosrb21@gmail.com>
2025-03-31 17:12:39 -0300

    testando deploy automatico

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -22,8 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if ($this->app->environment('production')) {
-            URL::forceRootUrl(config('https://mrfinancas.burghausen.com.br'));
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
         }
         // Event::listen(function (\SocialiteProviders\Manager\SocialiteWasCalled $event) {
         //     $event->extendSocialite('facebook', \SocialiteProviders\Facebook\Provider::class);
