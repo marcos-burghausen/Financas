fdb4bf0e63ae747e1ec6799b8e2b3c31d2c3ad92
marcos-burghausen <rafaelburghausen@gmail.com>
2025-06-30 21:38:09 -0300

    ajustes no vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,59 +1,31 @@
 import vue from "@vitejs/plugin-vue";
-import { fileURLToPath } from "node:url";
-import path from "path";
+import { fileURLToPath, URL } from "node:url";
 import { defineConfig, loadEnv } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
 
-    const env = loadEnv(mode, process.cwd(), "");
-
-    return {
-        plugins: [vue()],
+  return {
+    plugins: [vue()],
     server: {
-        host: true, // Importante para ser acessível dentro do Docker
-        allowedHosts: [
-            env.VITE_URL_DEV,
-        ],
-        proxy: {
-            // Qualquer requisição para /api será redirecionada
-            "/api": {
-                // target: 'http://backend_dev:80',
-                target: env.VITE_URL, // 
-                changeOrigin: true,
-            }
+      host: "0.0.0.0",
+      allowedHosts: [
+        env.VITE_URL_DEV
+      ],
+      proxy: {
+        "/api": {
+          target: env.VITE_URL,
+          changeOrigin: true,
         }
+      }
     },
     resolve: {
-        alias: {
-            "@": fileURLToPath(new URL("./src", import.meta.url)),
-            "@services": path.resolve(__dirname, "./src/services"),
-            "@types": path.resolve(__dirname, "./src/types"),
-            "@store": path.resolve(__dirname, "./src/store")
-        }
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+        "@services": fileURLToPath(new URL("./src/services", import.meta.url)),
+        "@types": fileURLToPath(new URL("./src/types", import.meta.url)),
+        "@store": fileURLToPath(new URL("./src/store", import.meta.url))
+      }
     }
-};
+  };
 });
-// import vue from "@vitejs/plugin-vue";
-// import { defineConfig } from "vite";
-
-// export default defineConfig({
-//     plugins: [vue()],
-//     resolve: {
-//         alias: {
-//             "@": path.resolve(__dirname, "./src"),
-//             // "@": fileURLToPath(new URL("./src", import.meta.url)),
-//             // "@components": fileURLToPath(new URL("./src/components", import.meta.url))
-//         }
-//     },
-//     build: {
-//         rollupOptions: {
-//             external: [], // Adicione aqui módulos que devem ser externalizados
-//             output: {
-//                 manualChunks: {
-//                     // Sua configuração de chunks...
-//                 }
-//             }
-//         }
-//     }
-// });
