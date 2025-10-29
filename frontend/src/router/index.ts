1f6503b88cafd9bdecac02a890483c54de2a1e33
marcos-burghausen <rafaelburghausen@gmail.com>
2025-10-28 22:36:10 -0300

    criando a rota login

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -14,6 +14,11 @@ const router = createRouter({
             component: () => import("../views/HomeView.vue"),
         },
         {
+            path: "/login",
+            name: "login",
+            component: () => import("../views/acesso/LoginView.vue"),
+        },
+        {
             path: "/cadastro",
             name: "cadastro",
             component: () => import("../views/acesso/CadastroView.vue"),
